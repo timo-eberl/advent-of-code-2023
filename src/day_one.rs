a3a05ba3629e945be10f12bb7e339aba8ce37e7b
Timo Eberl <timo@timoeberl.de>
2023-12-01 14:56:00 +0100

    Day One, Part One

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,3 +1,36 @@
-pub fn day_one(input : String) -> String {
-	input
+pub fn part_one(input : &String) -> u32 {
+	let lines = input.split('\n');
+
+	let mut sum = 0;
+
+	for line in lines {
+		// reached last line
+		if line.len() == 0 { return sum }
+
+		let mut two_digit_number_s = String::with_capacity(2);
+
+		for character in line.chars() {
+			if character.is_ascii_digit() {
+				two_digit_number_s.insert(0, character);
+				break
+			}
+		}
+
+		for character in line.chars().rev() {
+			if character.is_ascii_digit() {
+				two_digit_number_s.insert(1, character);
+				break
+			}
+		}
+
+		let two_digit_number = two_digit_number_s.parse::<u32>()
+			.expect("Could not parse line");
+		sum += two_digit_number;
+	}
+
+	sum
+}
+
+pub fn part_two(_input : &String) -> u32 {
+	0
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ fn main() {
     let file_path = "input/day_one";
     let contents = fs::read_to_string(file_path)
         .expect("Could not read input file");
-    println!("Day One:\n{}\n", aoc_2023::day_one::day_one(contents));
+    println!("Day One, Part One:\n{}\n", aoc_2023::day_one::part_one(&contents));
+    println!("Day One, Part Two:\n{}\n", aoc_2023::day_one::part_two(&contents));
 }
